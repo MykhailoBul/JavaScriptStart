664043195d2bdd8243a526cafa0338fecef421f5
MykhailoBul <mykhailo.bulava@voco.ee>
2025-10-15 05:11:16 -0700

    js

---
+++ index.js
@@ -0,0 +1,24 @@
+// console.log("Hello, World!");
+// console.log("I like TV");
+
+// window.alert('This is an aleert')
+// window.alert('I like TV')
+
+// document.getElementById("myH1").textContent = "Hello!";
+// document.getElementById("myP1").textContent = "I like TV";
+let x;
+x = 100;
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(x);
+console.log(`You are ${age} years old`);
+console.log(`Your price is $${price}`);
+console.log(`Your GPA is ${gpa}`);
+// This is a comment
+
+/*
+    This is a comment
+*/
