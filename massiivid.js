944ddf901770e840fe714a40e42eb4aeeb5ebbba
MykhailoBul <mykhailo.bulava@voco.ee>
2025-11-10 16:17:51 +0200

    Update massiivid.js

--- massiivid.js
+++ massiivid.js
@@ -98,15 +98,15 @@
 //     console.log("Goodbye");
 // }
 
-sum(displayPage, 1, 2)
+// sum(displayPage, 1, 2)
 
-function sum(callback, x, y){
-    let result = x + y;
-    callback(result);
-}
-function displayConsole(result){
-    console.log(result);
-}
-function displayPage(result){
-    document.getElementById("myH1").textContent = result;
-}
+// function sum(callback, x, y){
+//     let result = x + y;
+//     callback(result);
+// }
+// function displayConsole(result){
+//     console.log(result);
+// }
+// function displayPage(result){
+//     document.getElementById("myH1").textContent = result;
+// }
